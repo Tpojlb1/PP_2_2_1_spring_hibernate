24086fc9b5c12c3fefb6443c87743b0798955528
Tpojlb1 <118221543+Tpojlb1@users.noreply.github.com>
2023-01-12 02:13:40 +0700

    Update User.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -19,7 +19,7 @@ private String lastName;
 @Column(name = "email")
 private String email;
 
-@OneToOne(cascade = CascadeType.ALL)
+@OneToOne
 @JoinColumn(name = "car_id")
 private Car car;
 
@@ -27,61 +27,61 @@ public User() {
 }
 
 public User(String firstName, String lastName, String email, Car car) {
-   this.firstName = firstName;
-   this.lastName = lastName;
-   this.email = email;
-   this.car = car;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.car = car;
 }
 
 public Long getId() {
-   return id;
+    return id;
 }
 
 public void setId(Long id) {
-   this.id = id;
+    this.id = id;
 }
 
 public String getFirstName() {
-   return firstName;
+    return firstName;
 }
 
 public void setFirstName(String firstName) {
-   this.firstName = firstName;
+    this.firstName = firstName;
 }
 
 public String getLastName() {
-   return lastName;
+    return lastName;
 }
 
 public void setLastName(String lastName) {
-   this.lastName = lastName;
+    this.lastName = lastName;
 }
 
 public String getEmail() {
-   return email;
+    return email;
 }
 
 public void setEmail(String email) {
-   this.email = email;
+    this.email = email;
 }
 
 public Car getCar() {
-   return car;
+    return car;
 }
 
 public void setCar(Car car) {
-   this.car = car;
+    this.car = car;
 }
 
 @Override
 public String toString() {
-   return "User{" +
-           "id=" + id +
-           ", firstName='" + firstName + '\'' +
-           ", lastName='" + lastName + '\'' +
-           ", email='" + email + '\'' +
-           ", car=" + car +
-           '}';
+    return "User{" +
+            "id=" + id +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            ", email='" + email + '\'' +
+            ", car=" + car +
+            '}';
 }
 
 }
